e2a689ff04d1995106f0a65aac0da18d926df2d0
Omar-Lamprea <lampreaomar@gmail.com>
2023-07-19 16:42:41 -0500

    env added

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const typeOfPokemonRoutes = require('./routes/typePokemonRoutes')
 const pokemonRoutes = require('./routes/pokemonRoutes')
 
 const app = express()
 const port = 3000;
+const dbURI = process.env.MONGO_URL
 
 // Middleware para analizar el cuerpo de las solicitudes JSON
 app.use(bodyParser.json())
-
-//const dbURI = 'mongodb://localhost:27017/api-pokemon-db';
-const dbURI = 'mongodb+srv://lampreaomar:rw99chG0UXMs2HvO@cluster0.l3wdep4.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
